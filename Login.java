0143e19f250fd6160a98edec45ce1d8766e2c849
Praveena <praveleka@gmail.com>
2018-09-20 23:39:39 +0530

    simple code fix

--- Login.java
+++ Login.java
@@ -1,6 +1,7 @@
     class Login{  
         public static void main(String args[]){  
          System.out.println("Hello Java");
-system.out.println("hi SQL");  
+system.out.println("hi SQL");
+system.out.println("kiddu");  
         }  
     }  
